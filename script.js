aa69e6758708f58221fa20ccdd1c51a17e47af5f
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-05-23 02:08:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let numero = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isnumero(n) {
+    if (Number(n) > 0 && Number(n) < 101) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+        }
+    }
+
+function adicionar() {
+    if (isnumero(numero.value) && !inlista(numero.value, valores)) {
+        valores.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista!')
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        media = soma / total 
+        }   
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${total} números encontrados.</p>` 
+        res.innerHTML += `<p>O maior valor informado é ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado é ${menor}.</p>`
+        res.innerHTML += `<p>A soma de todos os valores é de ${soma}.</p>`
+        res.innerHTML += `<p>A média de todos os valores é de ${media}.</p>`
+        res.innerHTML += `<p>A média de todos os valores é de ${multi}.</p>`
+    }
+}
